5a289af08d3a688efc5ece6d0987fef303dbc6df
Vessel <josecarlosgonzalezaof@gmail.com>
2022-03-03 12:35:22 +0100

    802

--- 802/scripts/main.js
+++ 802/scripts/main.js
@@ -1,10 +1,21 @@
 
-const resultadoTexto = document.querySelector("#resolve");
-const buttonMultiply = document.querySelector("#multiply");
-const buttonDivision = document.querySelector("#division");
+let resultadoTexto = document.querySelector("#resolve");
+let buttonSum = document.querySelector("#sum");
+let buttonMultiply = document.querySelector("#multiply");
+let buttonDivision = document.querySelector("#division");
 
+buttonSum.onclick = sum;
 buttonMultiply.onclick = multiply;
 buttonDivision.onclick = division;
+
+function sum() {
+    let firstNumber = document.querySelector("#first").value;
+    let secondNumber = document.querySelector("#second").value;
+    firstNumber = parseInt(firstNumber,10);
+    secondNumber = parseInt(secondNumber,10);
+    resultadoTexto.textContent =  firstNumber + secondNumber;
+    resultadoTexto.after(firstNumber+" + "+secondNumber+" = "+resultadoTexto.textContent);
+}
 
 function multiply() {
     let firstNumber = document.querySelector("#first").value;
