7b246061c926ab60fb8bf61ed6b2efbeb4d04927
Vessel <josecarlosgonzalezaof@gmail.com>
2022-03-09 10:22:17 +0100

    805

---
+++ 805/scripts/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
+
+function main() {
+
+    let checked = [10]
+
+    function checkBox() {
+        let ul = document.createElement("ul");
+        document.body.append(ul);
+        let li = document.createElement("li");
+        ul.append(li);
+        let checkBox = document.createElement("input");
+        checkBox.setAttribute("type","checkbox");
+        li.append(checkBox);
+        let label = document.createElement("label");
+        label.setAttribute("for","number");
+        checkBox.after(label);
+        label.innerHTML = `${random(1, 10)}`;
+    }
+
+    for (let i=0; i < 10; i++) {
+        checked.unshift(checkBox());
+        
+    }
+    console.log(checked[2]);
+}
+
+document.addEventListener('DOMContentLoaded', main);
