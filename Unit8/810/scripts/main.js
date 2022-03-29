3d3a27be33fd5b1cb6dfda2517d9a87c2eaf5fab
Vessel <josecarlosgonzalezaof@gmail.com>
2022-03-29 09:45:47 +0200

    810

---
+++ Unit8/810/scripts/main.js
@@ -0,0 +1,34 @@
+let metamorfosis = {
+    title: 'La Metamorfosis',
+    author: 'Franz Kafka',
+    publishedIn: 1915,
+    alreadyRead: true,
+    cover: 'metamorfosis.jpg'
+};
+let cthulhu = {
+    title: 'La Llamada De Cthulhu',
+    author: 'H. P. Lovecraft',
+    publishedIn: 1928,
+    alreadyRead: false,
+    cover: 'cthulhu.jpg'
+};
+let berserk = {
+    title: 'Berserk',
+    author: 'Kentaro Miura',
+    publishedIn: 1989,
+    alreadyRead: true,
+    cover: 'berserk.jpg'
+};
+
+let harryPotter4 = {
+    title: 'Harry Potter 4',
+    author: 'J. K. Rowling',
+    publishedIn: 2000,
+    alreadyRead: false,
+    cover: 'harrypotter4.jpg'
+};
+
+
+
+let books = [metamorfosis, cthulhu, berserk];
+books.push(harryPotter4);
